fc9502d0c395613978547bd89df888c1b7dac399
shola-adegunle <shola.adegunle@yahoo.com>
2023-12-08 21:21:36 +0000

    updated bucket details

--- manifest-files/main.tf
+++ manifest-files/main.tf
@@ -5,6 +5,11 @@ terraform {
         version = ">=5.0"
     }
   }
+  backend "s3" {
+    bucket = "tf-state-sadey2k"
+    key    = "dev-tfstate"
+    region = "eu-west-2"
+  }
 }
 
 provider "aws" {
